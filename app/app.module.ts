75ea93d9c9a1ba3940446ee9e808d3ec9ae0a3f2
Filipe Silva <filipematossilva@gmail.com>
2016-11-14 22:23:28 +0000

    add hybrid

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,11 +1,32 @@
-import { NgModule }      from '@angular/core';
+declare var angular: angular.IAngularStatic;
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { UpgradeModule, downgradeComponent } from '@angular/upgrade/static';
 
-import { AppComponent }  from './app.component';
+import { MainController } from './main.controller';
+
+import { HeroDetailComponent } from './hero-detail.component';
 
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [
+    BrowserModule,
+    UpgradeModule
+  ],
+  declarations: [
+    HeroDetailComponent
+  ],
+  entryComponents: [
+    HeroDetailComponent
+  ]
 })
-export class AppModule { }
+export class AppModule {
+  ngDoBootstrap() {}
+}
+
+angular.module('heroApp', [])
+  .controller('MainController', MainController)
+  .directive('heroDetail', downgradeComponent({
+    component: HeroDetailComponent,
+    inputs: ['hero'],
+    outputs: ['deleted']
+  }) as angular.IDirectiveFactory);
